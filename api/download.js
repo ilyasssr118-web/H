47cd2177962ebc22f6d660d334f97a7d53ac8c8c
ilyasssr118-web <ilyasssr118@gmail.com>
2026-05-25 01:28:13 +0100

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,40 +1,86 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+const axios = require("axios");
+const cheerio = require("cheerio");
+const archiver = require("archiver");
 
-export default async function handler(req, res) {
-  try {
-    const { url } = req.query;
+module.exports = async (req, res) => {
 
-    if (!url) {
-      return res.status(400).send('No URL');
-    }
+try{
 
-    const { data } = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0',
-        'Referer': 'https://www.erome.com/'
-      }
-    });
+const album = req.query.url;
 
-    const $ = cheerio.load(data);
-
-    let images = [];
-    let videos = [];
-
-    $('img').each((i, el) => {
-      const src = $(el).attr('src');
-      if (src) images.push(src);
-    });
-
-    $('video source').each((i, el) => {
-      const src = $(el).attr('src');
-      if (src) videos.push(src);
-    });
-
-    res.status(200).json({ images, videos });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send(err.message);
-  }
+if(!album){
+return res.status(400).send("No URL");
 }
+
+const html = await axios.get(album,{
+headers:{
+"user-agent":"Mozilla/5.0"
+}
+});
+
+const $ = cheerio.load(html.data);
+
+let images = [];
+
+$("img").each((i,el)=>{
+
+const src = $(el).attr("src");
+
+if(
+src &&
+(
+src.includes(".jpg") ||
+src.includes(".jpeg") ||
+src.includes(".png")
+)
+){
+images.push(src);
+}
+
+});
+
+images = [...new Set(images)];
+
+res.setHeader(
+"Content-Type",
+"application/zip"
+);
+
+res.setHeader(
+"Content-Disposition",
+'attachment; filename="erome_images.zip"'
+);
+
+const archive = archiver("zip");
+
+archive.pipe(res);
+
+for(let i=0;i<images.length;i++){
+
+try{
+
+const img = await axios.get(images[i],{
+responseType:"arraybuffer",
+headers:{
+referer:"https://www.erome.com/"
+}
+});
+
+archive.append(
+img.data,
+{ name:`image_${i}.jpg` }
+);
+
+}catch(e){}
+
+}
+
+await archive.finalize();
+
+}catch(err){
+
+res.status(500).send(err.message);
+
+}
+
+};
