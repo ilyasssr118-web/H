1a72359d0ee425538471013c7921d51359363a8c
ilyasssr118-web <ilyasssr118@gmail.com>
2026-05-25 23:05:40 +0100

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -2,16 +2,17 @@ export default async function handler(req, res) {
   const { url } = req.query;
 
   if (!url) {
-    return res.status(400).send("No URL provided");
+    return res.status(400).send("Put ?url=");
   }
 
   try {
     const response = await fetch(url);
-    const data = await response.text();
+    const text = await response.text();
 
-    res.setHeader("Content-Type", "text/html");
-    res.status(200).send(data);
-  } catch (error) {
-    res.status(500).send("Error fetching URL");
+    res.setHeader("Content-Type", "text/plain; charset=utf-8");
+    res.status(200).send(text);
+
+  } catch (err) {
+    res.status(500).send("Error");
   }
 }
